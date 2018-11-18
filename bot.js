f6da53ceb4135b7339f38c1d3273853cdbf20abf
goldenshop12 <44838640+goldenshop12@users.noreply.github.com>
2018-11-18 15:10:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,5 +16,19 @@ client.on('message', message => {
 };    
 });
 
+client.on("message", message => {
+ 
+            if (message.content.startsWith(prefix + "bc2")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
+
 
  client.login(process.env.BOT_TOKEN); 
